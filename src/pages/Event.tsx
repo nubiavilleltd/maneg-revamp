e3aa8bfebdc26994dcb02b8aed0cfa271aaf9729
Maggy <edoziemagdalene@gmail.com>
2023-07-19 10:49:30 +0100

    events page

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import EventCard from '../components/EventCard';
+import arrow from '../assets/arrow-down.png';
+import '../styles/event.scss';
 
 function Event() {
-    return <div>event</div>;
+    return (
+        <div className='eventContainer'>
+            {/* Nav component */}
+            <div>
+                <div className='eventHeader'>
+                    <div>
+                        <h4>Latest Events</h4>
+                    </div>
+                    <div className='filterSection'>
+                        <p>Filter By</p>
+                        <div>
+                            <img src={arrow} alt='img' />
+                        </div>
+                    </div>
+                </div>
+                <div className='eventCardContainer'>
+                    <EventCard />
+                    <EventCard />
+                    <EventCard />
+                    <EventCard />
+                    <EventCard />
+                    <EventCard />
+                    <EventCard />
+                    <EventCard />
+                    <EventCard />
+                </div>
+            </div>
+            {/* footer component */}
+        </div>
+    );
 }
 
 export default Event;
