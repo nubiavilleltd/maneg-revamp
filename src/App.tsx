2f63c4da98fa79ad3d0d5cda96356901458531d1
Maggy <edoziemagdalene@gmail.com>
2023-07-19 08:30:12 +0100

    created about and event page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+// pages
+import Home from './pages/Home';
+import About from './pages/About';
+import Event from './pages/Event';
+
 function App() {
-  return (
-    <>
-      <h1>Hello From Nubiaville</h1>
-    </>
-  );
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/about' element={<About />} />
+                    <Route path='/event' element={<Event />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
