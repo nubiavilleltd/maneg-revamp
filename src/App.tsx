f55360e7d010af4403ae90c2a33771397a99904e
Maggy <edoziemagdalene@gmail.com>
2023-07-25 15:43:27 +0100

    event detail component

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,33 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 // pages
-import Home from "./pages/Home/Home";
-import About from "./pages/AboutPage/About";
-import Event from "./pages/EventPage/Event";
-import Navbar from "./components/Navbar/Navbar";
-import Footer from "./components/Footer/Footer";
-import News from "./pages/News";
-import NewsDetails from "./components/news/NewsDetails";
-import ScrollToTop from "./utils/ScrollToTop";
+import Home from './pages/Home/Home';
+import About from './pages/AboutPage/About';
+import Event from './pages/EventPage/Event';
+import EventDetail from './components/EventCard/EventDetail';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import News from './pages/News';
+import NewsDetails from './components/news/NewsDetails';
+import ScrollToTop from './utils/ScrollToTop';
 
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Navbar />
-        <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/event" element={<Event />} />
-          <Route path="/news" element={<News />} />
-          <Route path="/news/:newsID" element={<NewsDetails />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </>
-  );
+    return (
+        <>
+            <BrowserRouter>
+                <Navbar />
+                <ScrollToTop />
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/about' element={<About />} />
+                    <Route path='/event' element={<Event />} />
+                    <Route path='/event/:eventId' element={<EventDetail />} />
+                    <Route path='/news' element={<News />} />
+                    <Route path='/news/:newsID' element={<NewsDetails />} />
+                </Routes>
+                <Footer />
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
--- src/pages/EventPage/Event.tsx
+++ src/pages/EventPage/Event.tsx
@@ -1,38 +1,43 @@
-import EventCard from "../../components/EventCard/EventCard";
-import arrow from "../../assets/arrow-down.png";
-import "./event.scss";
+import EventCard from '../../components/EventCard/EventCard';
+import arrow from '/assets/arrow-down.png';
+import './event.scss';
+import eventData from './eventData';
 
 function Event() {
-  return (
-    <div className="eventContainer">
-      {/* Nav component */}
-      <div>
-        <div className="eventHeader">
-          <div>
-            <h4>Latest Events</h4>
-          </div>
-          <div className="filterSection">
-            <p>Filter By</p>
+    return (
+        <div className='eventContainer'>
+            {/* Nav component */}
             <div>
-              <img src={arrow} alt="img" />
+                <div className='eventHeader'>
+                    <div>
+                        <h4>Latest Events</h4>
+                    </div>
+                    <div className='filterSection'>
+                        <p>Filter By</p>
+                        <div>
+                            <img src={arrow} alt='img' />
+                        </div>
+                    </div>
+                </div>
+                <div className='eventCardContainer'>
+                    {eventData.map((event) => (
+                        <EventCard
+                            key={event.id}
+                            title={event.title}
+                            description={event.description}
+                            date={event.date}
+                            time={event.time}
+                            location={event.location}
+                            eventId={event.id}
+                            imgSrc={event.imgSrc}
+                            imgSrc2={event.imgSrc2}
+                        />
+                    ))}
+                </div>
             </div>
-          </div>
+            {/* footer component */}
         </div>
-        <div className="eventCardContainer">
-          <EventCard />
-          <EventCard />
-          <EventCard />
-          <EventCard />
-          <EventCard />
-          <EventCard />
-          <EventCard />
-          <EventCard />
-          <EventCard />
-        </div>
-      </div>
-      {/* footer component */}
-    </div>
-  );
+    );
 }
 
 export default Event;
