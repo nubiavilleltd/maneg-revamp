34d3b995cacb0810fd1092dc71ce7d3acaf15fa2
TryRaisins <tryraisins@gmail.com>
2023-07-26 15:54:05 +0100

    Added products page with some information and search functionality

---
+++ src/pages/Products/data/productData.ts
@@ -0,0 +1,38 @@
+import { Products } from "../../../types";
+import img1 from "../images/rice.jpg"
+import img2 from "../images/fish.jpg"
+import img3 from "../images/corn.jpg"
+import img4 from "../images/pencil.jpg"
+import img5 from "../images/wood.jpg"
+export const ProductsData: Products[] = [
+  {
+    id: 1,
+    image: img1,
+    title: "Rice",    
+  },
+  {
+    id: 2,
+    image: img2,
+    title: "Fish",
+  
+  },
+  {
+    id: 3,
+    image: img3,
+    title: "Corn",
+   
+  },
+  {
+    id: 4,
+    image: img4 ,
+    title: "Pencils",
+ 
+  },
+  {
+    id: 5,
+    image: img5,
+    title: "Wood",
+  
+  },
+ 
+];
--- src/pages/Products/products.tsx
+++ src/pages/Products/products.tsx
@@ -1,37 +1,70 @@
 
-import styles from "../Home/Home.module.scss";
-import Button from "../../components/UI/Button/Button";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faArrowRight,
-} from "@fortawesome/free-solid-svg-icons";
+import styles from "./products.module.scss";
+import { useEffect, useState } from "react";
+import { ProductsData } from "./data/productData";
+import { Link } from "react-router-dom";
+
 const Products = () =>{
-return<>
-<div className={styles["hero-section"]}>
-    <div className={styles["hero-first-col"]}>
-          <h1 className={styles["hero-heading"]}>
-            Sustainable Development & Job creation
-          </h1>
-          <p className={styles["hero-sub-heading"]}>
-            The importance of organized private sector groups in the growth and
-            development of Nigeria’s economy cannot be overstated. The
-            Manufacturers Association of Nigeria’s Export Promotion Group
-            represents the non-oil interests of private sector organizations
-            throughout the country.
-          </p>
-          <Button type="button" className={styles.button}>
-            Become a member
-            <FontAwesomeIcon icon={faArrowRight} className={styles.arrow} />
-          </Button>
-          <div className={styles.talk}>
-            Let's Talk
-            <Button type="button" className={styles.circleBtn}>
-              <FontAwesomeIcon icon={faArrowRight} />
-            </Button>
+    const [searchQuery, setSearchQuery] = useState("");
+  const [allProducts, setAllProducts] = useState(ProductsData);
+
+  useEffect(() => {
+    const filteredProducts = ProductsData.filter((products) =>
+      products.title.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    setAllProducts(filteredProducts);
+  }, [searchQuery]);
+return(
+ <section className={styles.product}>
+      <div className={styles.wrapper}>
+        <div className={styles.heading}>
+          <p style={{ fontWeight: "700", fontSize: "1.3rem" }}>Products</p>
+          <input
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            className={styles.search}
+            placeholder="Search by title..."
+          />
+        </div>
+
+        {searchQuery && (
+          <>
+            <h3 style={{ textAlign: "center", marginBottom: "1rem" }}>
+              Seach results for{" "}
+              <span style={{ color: "#21976f" }}>'{searchQuery}' </span>
+              <span>({allProducts.length} results)</span>
+            </h3>
+          </>
+        )}
+
+        {searchQuery && allProducts.length === 0 ? (
+          <div style={{ textAlign: "center", marginBottom: "1rem" }}>
+            <h2>No products found. Try searching something else.</h2>
           </div>
-        </div>
-        </div>
-</>
+        ) : (
+          <div className={styles["cards__wrapper"]}>
+            {allProducts.map((products) => (
+              <Link
+                to={`/products/${products.id}`}
+                key={products.id}
+                className={styles["card__container"]}
+              >
+                <img
+                  src={products.image}
+                  alt={products.title}
+                  className={styles.image}
+                />
+                <div className={styles["content__wrap"]}>
+                  <p className={styles.title}>{products.title}</p>
+                 
+                </div>
+              </Link>
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+)
 }
 
 export default Products
