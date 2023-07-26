6e605f94796a0281ab40706998656f252c4d0fdd
TryRaisins <tryraisins@gmail.com>
2023-07-26 16:40:27 +0100

    Added new products

--- src/pages/Products/data/productData.ts
+++ src/pages/Products/data/productData.ts
@@ -4,6 +4,10 @@ import img2 from "../images/fish.jpg"
 import img3 from "../images/corn.jpg"
 import img4 from "../images/pencil.jpg"
 import img5 from "../images/wood.jpg"
+import img6 from "../images/cocoa.jpg"
+import img7 from "../images/groundnut.jpg"
+import img8 from "../images/rubber.jpg"
+import img9 from "../images/textile.jpg"
 export const ProductsData: Products[] = [
   {
     id: 1,
@@ -32,6 +36,29 @@ export const ProductsData: Products[] = [
     id: 5,
     image: img5,
     title: "Wood",
+  
+  },
+  {
+    id: 6,
+    image: img6,
+    title: "Cocoa",
+  
+  },
+  {
+    id: 7,
+    image: img7,
+    title: "Groundnut",
+  
+  },{
+    id: 8,
+    image: img8,
+    title: "Rubber",
+  
+  },
+  {
+    id: 9,
+    image: img9,
+    title: "Textile",
   
   },
  
