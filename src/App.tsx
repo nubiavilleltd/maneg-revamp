d35b0926c154248396af4792c8f8e513d4c12606
Manuel-042 <ebukaezeanya14@gmail.com>
2023-07-27 08:42:19 +0100

    Member benefit sass file missing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 // pages
 import Home from "./pages/Home/Home";
 import About from "./pages/AboutPage/About";
@@ -10,24 +10,48 @@ import NewsDetails from "./components/news/NewsDetails";
 import ScrollToTop from "./utils/ScrollToTop";
 import Contact from "./pages/ContactPage/Contact";
 import MemberDirectory from "./pages/MemberDirectory/MemberDirectory";
+import EventDetail from "./components/EventCard/EventDetail";
+import Products from "./pages/Products/products";
+import Preloader from "./pages/Preloader/Preloader";
+import TransitionMemberBenefits from "./pages/MemberBenefits/MemberBenefits";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+  const location = useLocation();
+
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
-        <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/directory" element={<MemberDirectory />} />
-          <Route path="/event" element={<Event />} />
-          <Route path="/news" element={<News />} />
-          <Route path="/news/:newsID" element={<NewsDetails />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      {loading ? (
+        <Preloader />
+      ) : (
+        <div>
+          <Navbar />
+          <ScrollToTop />
+          <Routes location={location} key={location.pathname}>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/directory" element={<MemberDirectory />} />
+            <Route path="/event" element={<Event />} />
+            <Route path="/news" element={<News />} />
+            <Route path="/news/:newsID" element={<NewsDetails />} />
+            <Route path="/event/:eventId" element={<EventDetail />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/benefits" element={<TransitionMemberBenefits />} />
+          </Routes>
+          <Footer />
+        </div>
+      )}
     </>
   );
 }
